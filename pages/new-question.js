eff611bed38894a3739ef0044031103e3df13b35
emre kılıç <emrekilic87@gmail.com>
2020-11-15 18:19:19 +0300

    new question post data added

--- pages/new-question.js
+++ pages/new-question.js
@@ -2,17 +2,29 @@ import Layout from "../components/layout";
 import Head from "next/head";
 import css from "./New_question.module.css";
 
+let questionName = "";
+let choicesArr = [];
+
+function handleQuestion(e) {
+  questionName = e.target.value;
+}
+
 export async function postData() {
-  const rawResponse = await fetch("https://polls.apiblueprint.org/questions", {
+  let choices = document.querySelectorAll("input[data-attr='choice']");
+
+  choices.forEach(function (item) {
+    choicesArr.push(item.value);
+  });
+
+  await fetch("https://polls.apiblueprint.org/questions", {
     method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      /* this parameters static but question and choices should be changed when enter input areas */
-      question: 'example question',
-      choices: ["Swift", "Python", "Objective-C", "Ruby"],
+      question: questionName,
+      choices: choicesArr,
     }),
   });
 }
@@ -31,6 +43,7 @@ function NewQuestionPage() {
               className={`${css.input} ${css.question}`}
               placeholder="Question"
               required
+              onChange={handleQuestion}
             />
           </div>
           <div className={`${css.input_area} ${css.choice_area}`}>
@@ -39,6 +52,7 @@ function NewQuestionPage() {
               className={`${css.input} ${css.choice}`}
               placeholder="Choice"
               required
+              data-attr="choice"
             />
           </div>
           <div className={css.add_choice_area}>
